3979428574b7b9276f25e88d5afa432b4539678b
Arghavan Monajemi <monajemi.am@gmail.com>
2023-04-23 14:47:33 +0430

    support landscape.

--- app/src/main/java/com/android/calculator/MainActivity.java
+++ app/src/main/java/com/android/calculator/MainActivity.java
@@ -1,17 +1,18 @@
 package com.android.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.calculator.databinding.ActivityMainBinding;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private double number = 0, finalResult = 0;
-    private String operation;
+    private double finalNumber = 0, finalResult = 0;
+    private String operation, number;
     ActivityMainBinding binding;
 
     @Override
@@ -21,11 +22,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View view = binding.getRoot();
         setContentView(view);
 
+        if (savedInstanceState!=null){
+            binding.result.setText(savedInstanceState.getString(getString(R.string.saved_result_key),""));
+            binding.history.setText(savedInstanceState.getString(getString(R.string.saved_history_key), ""));
+            finalResult = Double.parseDouble(savedInstanceState.getString(getString(R.string.saved_result_key),"0"));
+        }
+
         binding.clearButton.setOnClickListener(v -> {
             binding.history.setText("");
             binding.result.setText("");
-            number = 0;
+            finalNumber = 0;
             finalResult = 0;
+            number = "";
+        });
+        binding.backButton.setOnClickListener(v -> {
+            if (number.length() > 1)
+                number = number.substring(0, number.length() - 2);
+            else number = "";
         });
         binding.equalButton.setOnClickListener(this);
         binding.plusButton.setOnClickListener(this);
@@ -46,19 +59,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(getString(R.string.saved_history_key), binding.history.getText().toString());
+        outState.putString(getString(R.string.saved_result_key), binding.result.getText().toString());
+    }
+
+    @Override
     public void onClick(View v) {
         String historyText = binding.history.getText().toString();
         String input = ((MaterialButton) v).getText().toString();
         binding.history.setText(String.format("%s%s", historyText, input));
         if (input.charAt(0) > 47 && input.charAt(0) < 58) {
-            number = number * 10 + Integer.parseInt(input);
+            finalNumber = finalNumber * 10 + Integer.parseInt(input);
         } else {
             if (finalResult != 0 || input.equals("=")) {
                 finalResult = calculate();
                 binding.result.setText((finalResult == (int) finalResult ? String.valueOf((int) finalResult) : String.valueOf(finalResult)));
             } else
-                finalResult = number;
-            number = 0;
+                finalResult = finalNumber;
+            finalNumber = 0;
             if (!input.equals("="))
                 operation = input;
             else {
@@ -72,19 +92,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         double result;
         switch (operation) {
             case "+":
-                result = finalResult + number;
+                result = finalResult + finalNumber;
                 break;
             case "-":
-                result = finalResult - number;
+                result = finalResult - finalNumber;
                 break;
             case "/":
-                result = finalResult / number;
+                result = finalResult / finalNumber;
                 break;
             case "×":
-                result = finalResult * number;
+                result = finalResult * finalNumber;
                 break;
             case "%":
-                result = finalResult % number;
+                result = finalResult % finalNumber;
                 break;
             default:
                 result = finalResult;
